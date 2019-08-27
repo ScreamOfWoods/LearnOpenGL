1d0015958565ae55b0678587db56392d7d39c403
Atanas Yordanov Shalaverov <ashalaverov@mm-sol.com>
2019-08-27 18:32:41 +0300

    Drawing using Vertex Array Object, Vertex Buffer Object and Vertex Element Object + Shaders

--- learnopengl/src/main.cpp
+++ learnopengl/src/main.cpp
@@ -3,6 +3,12 @@
 
 #include <iostream>
 
+const char* vertexShaderSource = 
+"#version 330 core\nlayout (location = 0) in vec3 aPos;\n\nvoid main()\n{\ngl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n}";
+
+const char* fragmentShaderSource = 
+"#version 330 core\nout vec4 FragColor;\n\nvoid main()\n{\nFragColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);\n}";
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
 	glViewport(0, 0, width, height);
@@ -41,6 +47,82 @@ int main()
 		return -1;
 	}
 
+	float vertices[] = {
+		-0.5f, 0.5f, 0.0f,
+		 0.25f, 0.25f, 0.0f,
+		 0.5f, -0.5f, 0.0f,
+		-0.25f, -0.25f, 0.0f
+	};
+
+	unsigned int indices[] = {
+		0, 1, 2,
+		0, 2, 3
+	};
+
+	GLuint VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), (GLvoid*) vertices, GL_STATIC_DRAW);
+
+	GLuint VAO;
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+	unsigned int EBO;
+	glGenBuffers(1, &EBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	int success;
+	char log[512];
+
+	GLuint vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	
+	if(!success) {
+		glGetShaderInfoLog(vertexShader, 512, NULL, log);
+		std::cout<<"Error::VERTEX::SHADER\n"<<log<<std::endl;
+	}
+
+	GLuint fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	
+	if(!success) {
+		glGetShaderInfoLog(vertexShader, 512, NULL, log);
+		std::cout<<"Error::FRAGMENT::SHADER\n"<<log<<std::endl;
+	}
+
+	GLuint shaderProgram;
+	shaderProgram = glCreateProgram();
+
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	
+	if(!success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, log);
+		std::cout<<"Error::SHADER::PROGRAM\n"<<log<<std::endl;
+	}
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (GLvoid*) 0);
+	glEnableVertexAttribArray(0);
+
+	glUseProgram(shaderProgram);
+	glBindVertexArray(VAO);
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
 	glViewport(0, 0, 800, 600);
 
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
@@ -51,6 +133,8 @@ int main()
 		glClearColor(0.5f, 0.2f, 0.3f, 0.3f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
