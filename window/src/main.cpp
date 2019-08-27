90056183942c4b4dad34fd9973cfe51657b5db2c
Atanas Yordanov Shalaverov <ashalaverov@mm-sol.com>
2019-08-27 15:06:46 +0300

    Successfully creating window with GLFW and GLAD

--- window/src/main.cpp
+++ window/src/main.cpp
@@ -1,9 +1,58 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include <stdio.h>
+
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window)
+{
+	if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, true);
+	}
+}
 
 int main()
 {
-	printf("Hello GL\n");
+	std::cout<<"Hello GL\n";
+
+	if(glfwInit() == GL_FALSE) {
+		std::cout<<"Failed to load GLFW!\n";
+		return -1;
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);	
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);	
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);	
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Hello GL", NULL, NULL);
+	if(window == NULL) {
+		std::cout<<"Failed to create GLFW window\n";
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cout<<"Failed to init GLAD\n";
+		return -1;
+	}
+
+	glViewport(0, 0, 800, 600);
+
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	
+	while(!glfwWindowShouldClose(window)) {
+		processInput(window);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	
+	glfwTerminate();
+
 	return 0;
 }
