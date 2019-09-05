fc1806d856993dc4b06513a706b7a84c2342329a
Atanas Yordanov Shalaverov <ashalaverov@mm-sol.com>
2019-09-05 14:43:01 +0300

    Commiting forgotten headers

--- learnopengl/include/shader.h
+++ learnopengl/include/shader.h
@@ -5,6 +5,9 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 class Shader
 {
@@ -20,6 +23,7 @@ class Shader
 		void setBool(const std::string &name, bool value) const;
 		void setInt(const std::string &name, int value) const;
 		void setFloat(const std::string &name, float value) const;
+		void setMatrix(const std::string &name, glm::mat4 transformationMatrix) const;
 		unsigned int getShaderID();
 };
 
---
+++ learnopengl/include/transformations.h
@@ -0,0 +1,17 @@
+#ifndef TRANSFORMATIONS_H
+#define TRANSFORMATIONS_H
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+
+glm::mat4 rotate(glm::mat4 transformationMatrix, glm::vec3 rotationAxis, float degree);
+
+glm::mat4 translate(glm::mat4 transformationMatrix, glm::vec3 position);
+
+glm::mat4 scale(glm::mat4 transformationMatrix, glm::vec3 multiplier);
+
+void applyTransformations(glm::mat4 transformationMatrix, Shader* shaderProgram, std::string uniformName);
+
+#endif //TRANSFORMATIONS_H
