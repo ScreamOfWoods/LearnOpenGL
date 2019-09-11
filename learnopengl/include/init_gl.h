bd73d8dd4b84a99e309f14b2363131f1de5610aa
Atanas Yordanov Shalaverov <ashalaverov@mm-sol.com>
2019-09-11 11:54:23 +0300

    Added initialization for simplycity

---
+++ learnopengl/include/init_gl.h
@@ -0,0 +1,8 @@
+#ifndef INIT_GL_H
+#define INIT_GL_H
+
+GLFWwindow* createWindow(int major, int minor, std::string hint, int width, int height);
+
+void initWorkspace(int majorVersion, int minorVersion, std::string hint, int width, int height);
+
+#endif //INIT_GL_H
---
+++ learnopengl/src/init_gl.cpp
@@ -0,0 +1,42 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <stdlib.h>
+
+#include <init_gl.h>
+
+GLFWwindow* createWindow(int major, int minor, std::string hint, int width, int height)
+{
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, major);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, minor);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(width, height, hint.c_str(), NULL, NULL);
+	return window;
+	
+}
+
+void initWorkspace(int majorVersion, int minorVersion, std::string hint, int width,int height)
+{
+	if(glfwInit() == GL_FALSE) {
+		std::cerr<<"Failed to init GLFW!\n";
+		exit(EXIT_FAILURE);
+	}
+	
+	GLFWwindow* window;
+	window = createWindow(majorVersion, minorVersion, hint, width, height);
+	if(window == NULL) {
+		std::cerr<<"Failed to create GLFW window!\n";
+		exit(EXIT_FAILURE);
+	}
+
+	glfwMakeContextCurrent(window);
+
+	if(!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+		std::cerr<<"Failed to init GLAD!\n";
+		exit(EXIT_FAILURE);
+	}
+
+	glEnable(GL_DEPTH_TEST);
+}
