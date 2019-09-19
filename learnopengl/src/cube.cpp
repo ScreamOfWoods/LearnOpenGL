806477cca1bbe583731476979642e09b3cd836bf
Atanas Yordanov Shalaverov <ashalaverov@mm-sol.com>
2019-09-19 11:43:41 +0300

    Multiple cubes and a rectangle rotating

--- learnopengl/src/cube.cpp
+++ learnopengl/src/cube.cpp
@@ -88,6 +88,20 @@ int main()
 		5, 1, 2, 5, 2, 6
 	};
 
+	glm::vec3 cubePositions[] = {
+		glm::vec3( 0.0f,  0.0f,  0.0f), 
+		glm::vec3( 2.0f,  5.0f, -15.0f), 
+		glm::vec3(-1.5f, -2.2f, -2.5f),  
+		glm::vec3(-3.8f, -2.0f, -12.3f),  
+		glm::vec3( 2.4f, -0.4f, -3.5f),  
+		glm::vec3(-1.7f,  3.0f, -7.5f),  
+		glm::vec3( 1.3f, -2.0f, -2.5f),  
+		glm::vec3( 1.5f,  2.0f, -2.5f), 
+		glm::vec3( 1.5f,  0.2f, -1.5f), 
+		glm::vec3(-1.3f,  1.0f, -1.5f)  
+	};
+
+
 	VertexArray cubeVAO;
 	VertexLayout cubeLayout;
 	cubeLayout.push(3);
@@ -114,18 +128,31 @@ int main()
 			std::cout<<"Unable to find UNIFORM variable from SHADER\n";
 			exit(EXIT_FAILURE);
 		}
-		setClippingPlane(redShader, 45.0f, glm::vec3(1.0f, 0.0f, 0.0f), -55.0f,
+		setClippingPlane(redShader, 45.0f, glm::vec3(0.5f, 1.0f, 0.0f), -55.0f * (float)glfwGetTime(),
 			glm::vec3(0.0f, 0.0f, -3.0f), (float) width / (float) height, 0.1f, 100.0f);
 
 		glUniform4f(rcolorLocation, rpulse, 0.0f, 0.0f, 1.0f);
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-
+		
 		cubeShader->use();
-		setClippingPlane(cubeShader, 35.0f, glm::vec3(1.0f, 0.0f, 0.0f), -55.0f,
-			glm::vec3(0.0f, 0.0f, -7.0f), (float) width / (float) height, 0.1f, 100.0f);
-
+		
+		glm::mat4 view = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
+		glm::mat4 projection = glm::mat4(1.0f);
+		projection = glm::perspective(glm::radians(45.0f), (float)width / (float)height, 0.1f, 100.0f);
+		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+		cubeShader->setMatrix("projection", projection);
+		cubeShader->setMatrix("view", view);
+		
 		cubeVAO.bind();
-		glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+		for(int i = 0; i < 10; i++) {
+			glm::mat4 model = glm::mat4(1.0f);
+			model = glm::translate(model, cubePositions[i]);
+			float angle = 20.0f * (i + 1) * glfwGetTime();
+			model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+			cubeShader->setMatrix("model", model);
+
+			glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+		}
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
